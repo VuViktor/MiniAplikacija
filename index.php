f54355e4986e50d8c85e927db822c44455add904
VuViktor <95453670+VuViktor@users.noreply.github.com>
2021-12-07 13:20:13 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php
+session_start();
+
+  $_SESSION;
+ ?>
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Pocetna strana</title>
+  </head>
+  <body>
+    <a href="login.php"> Odjava</a>
+    <h1>Pocetna strana</h1>
+
+    <br>
+    Dobro došli!
+  </body>
+</html>
---
+++ konekcija.php
@@ -0,0 +1,14 @@
+<?php
+
+$dbhost="localhost";
+$dbhost="root";
+$dbpass="";
+$dbname="login_sample_db";
+
+if(!$con=mysqli_connect($dbhost,$dbhost,$dbpass,$dbname)){
+
+  die("Konekcija nije uspela!");
+}
+
+
+ ?>
